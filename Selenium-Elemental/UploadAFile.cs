94664efa0f60aba4cf83e3ba54bb451e88d64877
jtay2380 <jtaylor@askcts.com>
2015-01-20 19:59:26 -0600

    Added .Net Selenium Tips

---
+++ Selenium-Elemental/UploadAFile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Selenium_Elemental
+{
+    [TestClass]
+    public class UploadAFile
+    {
+        public static IWebDriver Driver { get; set; }
+        private TestContext testContextInstance;
+
+        #region Test Context Setup
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        #endregion
+
+
+        [TestMethod]
+        public void UploadFile()
+        {
+        }
+
+        [TestInitialize]
+        public void StartTest()
+        {
+            Driver = new FirefoxDriver();
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/upload");
+        }
+
+        [TestCleanup]
+        public void EndTest()
+        {
+            Driver.Quit();
+        }
+
+
+
+    }
+}
